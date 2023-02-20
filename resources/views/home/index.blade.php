6d7452090f84baa95eaa761c86003baab7b177cc
rayhan-al-shorif <rayhanalshorif4@gmail.com>
2023-02-20 14:26:36 +0600

    restyle website

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,14 +7,15 @@
 @endsection
 
 @section('content')
-@include('home._partials.navbar')
-@include('home._partials.slider')
-@include('home._partials.what-we-do')
+{{-- @include('home._partials.navbar') --}}
+{{-- @include('home._partials.slider') --}}
 @include('home._partials.about')
+@include('home._partials.what-we-do')
 @include('home._partials.service')
+@include('home._partials.about_sir')
 @include('home._partials.contact')
 @include('home._partials.footer')
-@include('home._partials.settings')
+{{-- @include('home._partials.settings') --}}
 @include('home._partials.social')
 @endsection
 @push('scripts')
